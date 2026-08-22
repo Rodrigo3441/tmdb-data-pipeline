345417a1b6f9f59324da555df8c56aab78151c31
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-21 22:52:59 -0300

    add a new class for movie genre data request

--- src/api_interface.py
+++ src/api_interface.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class TheMovieDatabase:
-    api_url = os.getenv('API_URL')
+class TheMovieDB_movies:
+    api_url = os.getenv('API_MOVIE_URL')
     api_token = os.getenv('API_TOKEN')
 
     def __init__ (self):
@@ -20,10 +20,23 @@ class TheMovieDatabase:
 
         return result['total_pages']
 
-
     def return_movie_page(self, page_number: int):
         return self.session.get(
                 self.api_url,
                 params={'page': page_number},
                 timeout=10
+        )
+
+class TheMovieDB_genres:
+    api_url = os.getenv('API_GENRE_URL')
+    api_token = os.getenv('API_TOKEN')
+
+    def __init__ (self):
+        self.session = requests.Session()
+        self.session.headers.update({'Authorization': f'Bearer {self.api_token}'})
+
+    def return_genre_data(self):
+        return self.session.get(
+            self.api_url,
+            timeout=10
         )
