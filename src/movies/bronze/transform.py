5683927f3dfd7b5080a1c2063ab82629abf69a77
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-21 23:00:47 -0300

    add comments for the script

--- src/movies/bronze/transform.py
+++ src/movies/bronze/transform.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 def run(raw_data: requests.Response) -> dict:
     cleaned_data = raw_data.json()
+
+    # keep only the result columns for the dataframe
     cleaned_data = pd.json_normalize(cleaned_data, record_path=['results'])
+
+    # formats the genre_ids object to string
     cleaned_data['genre_ids'] = cleaned_data['genre_ids'].astype('str')
 
     return {
