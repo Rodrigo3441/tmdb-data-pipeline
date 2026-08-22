f7ab2f959b355230dd903d37e706de864535d870
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-21 23:03:40 -0300

    add the initial version of gold layer transform orchestrator

---
+++ src/gold/transform.py
@@ -0,0 +1,7 @@
+from src.gold.transformations import dim_genres
+from src.gold.transformations import dim_movies
+from src.gold.transformations import fact_movies
+import pandas as pd
+
+def run(data: dict) -> dict:
+    pass
