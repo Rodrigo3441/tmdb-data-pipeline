47e02e93d005cc73a03489677f0dd63fed1501a1
Rodrigo3441 <119820531+Rodrigo3441@users.noreply.github.com>
2026-08-21 23:00:29 -0300

    remove unused function from the script and update class name

--- src/movies/bronze/extract.py
+++ src/movies/bronze/extract.py
@@ -1,13 +1,6 @@
 from src import api_interface as movieApi
 import requests
-import pandas as pd
 
 def run(page: int) -> requests.Response:
-    moviedb = movieApi.TheMovieDatabase() 
+    moviedb = movieApi.TheMovieDB_movies() 
     return moviedb.return_movie_page(page)
-
-
-def return_total_pages() -> int:
-    moviedb = movieApi.TheMovieDatabase()
-    total_pages = moviedb.return_total_pages()
-    return total_pages
